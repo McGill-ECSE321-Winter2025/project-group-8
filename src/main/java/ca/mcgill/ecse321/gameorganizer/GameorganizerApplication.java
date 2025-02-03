f2accb4477f76122589976117bf191535479313a
Plazma <118921526+PlazmaMamba@users.noreply.github.com>
2025-02-03 11:12:00 -0500

    Add files via upload
    initial file creation

---
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/GameorganizerApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.gameorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameorganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameorganizerApplication.class, args);
+	}
+
+}
