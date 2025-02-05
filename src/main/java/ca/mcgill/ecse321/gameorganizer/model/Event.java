652d9f1f7406aedc3efda3eb819a8da85107729d
PlazmaMamba <alexander.kudinov@protonmail.com>
2025-02-04 19:56:38 -0500

    Feature: added BorrowRequest, Event, Game, LendingRecord, Registration, Review classes from model, without JPA annotation

--- src/main/java/ca/mcgill/ecse321/gameorganizer/model/Event.java
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/model/Event.java
@@ -1,4 +1,173 @@
 package ca.mcgill.ecse321.gameorganizer.model;
 
-public class Event {
+import java.util.Date;
+
+public class Event
+{
+
+
+    private static int nextId = 1;
+
+
+
+    //Event Attributes
+    private String title;
+    private Date dateTime;
+    private String location;
+    private String description;
+    private int maxParticipants;
+
+
+    private int id;
+
+    //Event Associations
+    private Account createdBy;
+    private Game featuredGame;
+
+
+
+    public Event(String aTitle, Date aDateTime, String aLocation, String aDescription, int aMaxParticipants, Account aCreatedBy, Game aFeaturedGame)
+    {
+        title = aTitle;
+        dateTime = aDateTime;
+        location = aLocation;
+        description = aDescription;
+        maxParticipants = aMaxParticipants;
+        id = nextId++;
+        if (!setCreatedBy(aCreatedBy))
+        {
+            throw new RuntimeException("Unable to create Event due to aCreatedBy. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+        if (!setFeaturedGame(aFeaturedGame))
+        {
+            throw new RuntimeException("Unable to create Event due to aFeaturedGame. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+    }
+
+
+
+    public boolean setTitle(String aTitle)
+    {
+        boolean wasSet = false;
+        title = aTitle;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setDateTime(Date aDateTime)
+    {
+        boolean wasSet = false;
+        dateTime = aDateTime;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setLocation(String aLocation)
+    {
+        boolean wasSet = false;
+        location = aLocation;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setDescription(String aDescription)
+    {
+        boolean wasSet = false;
+        description = aDescription;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setMaxParticipants(int aMaxParticipants)
+    {
+        boolean wasSet = false;
+        maxParticipants = aMaxParticipants;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public Date getDateTime()
+    {
+        return dateTime;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public int getMaxParticipants()
+    {
+        return maxParticipants;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public Account getCreatedBy()
+    {
+        return createdBy;
+    }
+
+    public Game getFeaturedGame()
+    {
+        return featuredGame;
+    }
+
+    public boolean setCreatedBy(Account aNewCreatedBy)
+    {
+        boolean wasSet = false;
+        if (aNewCreatedBy != null)
+        {
+            createdBy = aNewCreatedBy;
+            wasSet = true;
+        }
+        return wasSet;
+    }
+
+    public boolean setFeaturedGame(Game aNewFeaturedGame)
+    {
+        boolean wasSet = false;
+        if (aNewFeaturedGame != null)
+        {
+            featuredGame = aNewFeaturedGame;
+            wasSet = true;
+        }
+        return wasSet;
+    }
+
+    public void delete()
+    {
+        createdBy = null;
+        featuredGame = null;
+    }
+
+
+
+
+
+    public String toString()
+    {
+        return super.toString() + "["+
+                "id" + ":" + getId()+ "," +
+                "title" + ":" + getTitle()+ "," +
+                "location" + ":" + getLocation()+ "," +
+                "description" + ":" + getDescription()+ "," +
+                "maxParticipants" + ":" + getMaxParticipants()+ "]" + System.getProperties().getProperty("line.separator") +
+                "  " + "dateTime" + "=" + (getDateTime() != null ? !getDateTime().equals(this)  ? getDateTime().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+                "  " + "createdBy = "+(getCreatedBy()!=null?Integer.toHexString(System.identityHashCode(getCreatedBy())):"null") + System.getProperties().getProperty("line.separator") +
+                "  " + "featuredGame = "+(getFeaturedGame()!=null?Integer.toHexString(System.identityHashCode(getFeaturedGame())):"null");
+    }
 }
--- src/main/java/ca/mcgill/ecse321/gameorganizer/model/Game.java
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/model/Game.java
@@ -1,4 +1,149 @@
 package ca.mcgill.ecse321.gameorganizer.model;
 
-public class Game {
+import java.util.Date;
+
+public class Game
+{
+
+
+
+    private static int nextId = 1;
+
+
+
+
+    private String name;
+    private int minPlayers;
+    private int maxPlayers;
+    private String image;
+    private Date dateAdded;
+
+
+    private int id;
+
+
+    private GameOwner owner;
+
+
+
+    public Game(String aName, int aMinPlayers, int aMaxPlayers, String aImage, Date aDateAdded, GameOwner aOwner)
+    {
+        name = aName;
+        minPlayers = aMinPlayers;
+        maxPlayers = aMaxPlayers;
+        image = aImage;
+        dateAdded = aDateAdded;
+        id = nextId++;
+        if (!setOwner(aOwner))
+        {
+            throw new RuntimeException("Unable to create Game due to aOwner. See https://manual.umple.org?RE002ViolationofAssociationMultiplicity.html");
+        }
+    }
+
+
+
+    public boolean setName(String aName)
+    {
+        boolean wasSet = false;
+        name = aName;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setMinPlayers(int aMinPlayers)
+    {
+        boolean wasSet = false;
+        minPlayers = aMinPlayers;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setMaxPlayers(int aMaxPlayers)
+    {
+        boolean wasSet = false;
+        maxPlayers = aMaxPlayers;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setImage(String aImage)
+    {
+        boolean wasSet = false;
+        image = aImage;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public boolean setDateAdded(Date aDateAdded)
+    {
+        boolean wasSet = false;
+        dateAdded = aDateAdded;
+        wasSet = true;
+        return wasSet;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getMinPlayers()
+    {
+        return minPlayers;
+    }
+
+    public int getMaxPlayers()
+    {
+        return maxPlayers;
+    }
+
+    public String getImage()
+    {
+        return image;
+    }
+
+    public Date getDateAdded()
+    {
+        return dateAdded;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public GameOwner getOwner()
+    {
+        return owner;
+    }
+
+    public boolean setOwner(GameOwner aNewOwner)
+    {
+        boolean wasSet = false;
+        if (aNewOwner != null)
+        {
+            owner = aNewOwner;
+            wasSet = true;
+        }
+        return wasSet;
+    }
+
+    public void delete()
+    {
+        owner = null;
+    }
+
+
+
+    public String toString()
+    {
+        return super.toString() + "["+
+                "id" + ":" + getId()+ "," +
+                "name" + ":" + getName()+ "," +
+                "minPlayers" + ":" + getMinPlayers()+ "," +
+                "maxPlayers" + ":" + getMaxPlayers()+ "," +
+                "image" + ":" + getImage()+ "]" + System.getProperties().getProperty("line.separator") +
+                "  " + "dateAdded" + "=" + (getDateAdded() != null ? !getDateAdded().equals(this)  ? getDateAdded().toString().replaceAll("  ","    ") : "this" : "null") + System.getProperties().getProperty("line.separator") +
+                "  " + "owner = "+(getOwner()!=null?Integer.toHexString(System.identityHashCode(getOwner())):"null");
+    }
 }
