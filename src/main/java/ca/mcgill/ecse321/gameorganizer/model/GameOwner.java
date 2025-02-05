4a32a24e748a87a46e3e378b64bcc2773d3ac32e
PlazmaMamba <alexander.kudinov@protonmail.com>
2025-02-04 19:05:21 -0500

    Feature: added initial versions of Account and GameOwner classes

--- src/main/java/ca/mcgill/ecse321/gameorganizer/model/GameOwner.java
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/model/GameOwner.java
@@ -1,4 +1,19 @@
 package ca.mcgill.ecse321.gameorganizer.model;
 
-public class GameOwner {
+public class GameOwner extends Account
+{
+
+
+    public GameOwner(String aName, String aEmail, String aPassword)
+    {
+        super(aName, aEmail, aPassword);
+    }
+
+
+    public void delete()
+    {
+        super.delete();
+    }
+
+
 }
