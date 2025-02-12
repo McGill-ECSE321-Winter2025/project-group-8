435a296d755305394142d138ac9f7a2040c09f29
Yessine-glitch <yessinechaari33@gmail.com>
2025-02-12 16:42:03 -0500

    implemented repositeries

--- src/main/java/ca/mcgill/ecse321/gameorganizer/repositories/EventRepository.java
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/repositories/EventRepository.java
@@ -4,6 +4,9 @@ import ca.mcgill.ecse321.gameorganizer.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
+    Optional<Event> findEventById(int id);
 }
--- src/main/java/ca/mcgill/ecse321/gameorganizer/repositories/RegistrationRepository.java
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/repositories/RegistrationRepository.java
@@ -4,6 +4,9 @@ import ca.mcgill.ecse321.gameorganizer.models.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RegistrationRepository extends JpaRepository<Registration, Integer> {
+    Optional<Registration> findRegistrationById(int id);
 }
