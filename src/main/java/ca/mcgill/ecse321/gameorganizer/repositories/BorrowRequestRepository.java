89dc454240efcfb853198275f970c5f91c968783
rayanBaida <ryanbaida@gmail.com>
2025-02-13 11:37:12 -0500

    Feature: BorrowRequest service layer

--- src/main/java/ca/mcgill/ecse321/gameorganizer/repositories/BorrowRequestRepository.java
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/repositories/BorrowRequestRepository.java
@@ -2,13 +2,20 @@ package ca.mcgill.ecse321.gameorganizer.repositories;
 
 import ca.mcgill.ecse321.gameorganizer.models.BorrowRequest;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BorrowRequestRepository extends JpaRepository<BorrowRequest, Integer> {
     Optional<BorrowRequest> findBorrowRequestById(int id);
+    
+    List<BorrowRequest> findOverlappingApprovedRequests(@Param("gameId") int gameId, 
+                                                        @Param("startDate") Date startDate, 
+                                                        @Param("endDate") Date endDate);
 }
 
