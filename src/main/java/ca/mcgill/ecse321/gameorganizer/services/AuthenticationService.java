e41f4cd4ee6ae7291623dd0679fd8a3ed5ca2964
dyune <david.wang5@mail.mcgill.ca>
2025-03-13 14:05:44 -0400

    FEATURE: Authentication feature setup

---
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/services/AuthenticationService.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse321.gameorganizer.services;
+
+import org.springframework.stereotype.Service;
+
+// TODO: Check out middleware package, authentication depends on that too
+
+@Service
+public class AuthenticationService {
+}
