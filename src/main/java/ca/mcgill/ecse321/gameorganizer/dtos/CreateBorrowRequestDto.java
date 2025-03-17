ade92eb000470a0e7302a015b53cdb1a4c7b30de
rayanBaida <ryanbaida@gmail.com>
2025-03-17 18:29:57 -0400

    Javadocs for Dto's

--- src/main/java/ca/mcgill/ecse321/gameorganizer/dtos/CreateBorrowRequestDto.java
+++ src/main/java/ca/mcgill/ecse321/gameorganizer/dtos/CreateBorrowRequestDto.java
@@ -2,13 +2,26 @@ package ca.mcgill.ecse321.gameorganizer.dtos;
 
 import java.util.Date;
 
+/**
+ * DTO for creating a borrow request.
+ * Contains only the necessary details needed to submit a new request.
+ * 
+ * @autor Rayan Baida
+ */
 public class CreateBorrowRequestDto {
     private int requesterId;
     private int requestedGameId;
     private Date startDate;
     private Date endDate;
 
-    // Constructor
+    /**
+     * Constructs a new create borrow request DTO.
+     * 
+     * @param requesterId ID of the user making the request.
+     * @param requestedGameId ID of the game being requested.
+     * @param startDate Start date of the borrow period.
+     * @param endDate End date of the borrow period.
+     */
     public CreateBorrowRequestDto(int requesterId, int requestedGameId, Date startDate, Date endDate) {
         this.requesterId = requesterId;
         this.requestedGameId = requestedGameId;
@@ -16,9 +29,15 @@ public class CreateBorrowRequestDto {
         this.endDate = endDate;
     }
 
-    // Getters
+    /** @return The ID of the requester. */
     public int getRequesterId() { return requesterId; }
+    
+    /** @return The ID of the requested game. */
     public int getRequestedGameId() { return requestedGameId; }
+    
+    /** @return The start date of the borrow period. */
     public Date getStartDate() { return startDate; }
+    
+    /** @return The end date of the borrow period. */
     public Date getEndDate() { return endDate; }
 }
